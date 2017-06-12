cad5e6520245344e788b340c2d6f640cf1307f71
Kari Newhouse <newhousemusic@gmail.com>
2017-06-12 16:16:12 -0700

    Create stamp objects and pull them out of coredata

--- StampViewController.swift
+++ StampViewController.swift
@@ -27,10 +27,28 @@ class StampViewController: UIViewController, UIImagePickerControllerDelegate, UI
         
         present(imagePicker, animated: true, completion: nil)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        stampImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
     @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let stamp = Stamp(context: context)
+        stamp.title = titleTextField.text
+        stamp.image = UIImagePNGRepresentation(stampImageView.image!)! as NSData
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
     }
 }
--- ViewController.swift
+++ ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  StampCollector
@@ -8,11 +9,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var stamps : [Stamp] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            stamps = try context.fetch(Stamp.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stamps.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let stamp = stamps[indexPath.row]
+        cell.textLabel?.text = stamp.title
+        cell.imageView?.image = UIImage(data: stamp.image as! Data)
+        return cell
 
+    }
 }
 
